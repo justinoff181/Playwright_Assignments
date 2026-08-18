ef344353447c0139bf4f5ca4d29045c1e48f0b57
justinoff181 <justinoff181@gmail.com>
2026-08-18 21:47:30 +0530

    Day1_Breakout_Assignment
    Day1_Breakout_Assignment

---
+++ 03-Breakout-assignment.js
@@ -0,0 +1,27 @@
+var firstname = "Raj" //Initialization
+console.log(firstname) //Raj 
+console.log(typeof firstname)
+
+var CompanyName = "TestLeaf" //Initialization
+console.log(CompanyName) // 
+console.log(typeof CompanyName)
+
+var MobileNumber = "9988774455" //Initialization
+console.log(MobileNumber) // 
+console.log(typeof MobileNumber)
+
+var IsAutomation =  true //Initialization
+console.log(IsAutomation) // 
+console.log(typeof IsAutomation)
+
+var hasplaywright =  undefined //Initialization
+console.log(hasplaywright) // 
+console.log(typeof hasplaywright)
+
+
+
+
+
+
+
+
